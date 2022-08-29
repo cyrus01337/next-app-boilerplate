cd44f35da3f00478953eaa0fc29f56d0ff377128
cyrus01337 <cyrus21337@gmail.com>
2022-08-29 13:11:08 +0100

    Rewrite boilerplate

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import Image from "next/image";
+
+import Layout from "../components/Layout";
+
+export default function Home() {
+    return (
+        <Layout>
+            <main>aaaaaaaaaaaaa</main>
+        </Layout>
+    );
+}
