611a3b0cf00faa0fcf51e0a52dde70e3ebb9ee3b
cyrus01337 <cyrus21337@gmail.com>
2022-08-29 15:20:25 +0100

    Add TailwindCSS formatting support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 const tailwindCSSTypography = require("@tailwindcss/typography");
+const prettierPluginTailwindCSS = require("prettier-plugin-tailwindcss");
 
 module.exports = {
     content: ["components/**/*.jsx", "pages/**/*.jsx"],
-    plugins: [tailwindCSSTypography],
+    plugins: [tailwindCSSTypography, prettierPluginTailwindCSS],
     theme: {
         extend: {},
     },
