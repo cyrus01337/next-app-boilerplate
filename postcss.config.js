378e0720a5649a01832060076ee3db4c5dacaf77
cyrus01337 <cyrus01337@gmail.com>
2022-09-02 11:29:34 +0100

    Result of auto-formatting

--- postcss.config.js
+++ postcss.config.js
@@ -1,32 +1,32 @@
-{
-    "plugins": [
+module.exports = {
+    plugins: [
         "postcss-flexbugs-fixes",
         "tailwindcss",
         [
             "postcss-preset-env",
             {
-                "autoprefixer": {
-                    "flexbox": "no-2009"
+                autoprefixer: {
+                    flexbox: "no-2009",
                 },
-                "features": {
-                    "custom-properties": false
+                features: {
+                    "custom-properties": false,
                 },
-                "stage": 3
-            }
+                stage: 3,
+            },
         ],
         [
             "postcss-font-magician",
             {
-                "display": "swap",
-                "variants": {
-                    "Inter": {
-                        "400": [],
+                display: "swap",
+                variants: {
+                    Inter: {
+                        400: [],
                         "400 bold": [],
-                        "700": [],
-                        "700 bold": []
-                    }
-                }
-            }
-        ]
-    ]
-}
+                        700: [],
+                        "700 bold": [],
+                    },
+                },
+            },
+        ],
+    ],
+};
