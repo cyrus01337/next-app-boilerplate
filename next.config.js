0ca791ebb0c4aa901c10820c62cfe7ca55e7487e
cyrus01337 <cyrus01337@gmail.com>
2022-09-23 09:27:48 +0100

    Shorten relative module resolution, change how default font is loaded in and niceties

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
+    webpack(config, { webpack }) {
+        config.resolve.alias["@"] = path.resolve("./src");
+
+        return config;
+    },
 };
 
 module.exports = nextConfig;
