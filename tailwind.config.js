ccff882ee706540841934455e143334917b5a9da
cyrus01337 <cyrus21337@gmail.com>
2022-09-23 09:35:31 +0100

    No more PostCSS config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 const tailwindCSSTypography = require("@tailwindcss/typography");
 
 module.exports = {
     content: ["src/**/*.jsx"],
     plugins: [tailwindCSSTypography],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ["Inter", ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
 };
