1b5229f2e3fa0abf931988f7da1b4d59dd5ae27a
cyrus01337 <cyrus01337@gmail.com>
2023-01-06 12:00:43 +0000

    Include autocomplete

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 const tailwindCSSTypography = require("@tailwindcss/typography");
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["src/**/*.jsx"],
     plugins: [tailwindCSSTypography],
