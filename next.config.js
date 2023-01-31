4530cbb7d75eb8dac5b7303616bdc05b5080984f
cyrus01337 <cyrus21337@gmail.com>
2023-01-31 12:29:39 +0000

    Opt for jsconfig over internal webpack mingling

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
-const path = require("path");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
-    webpack(config, { webpack }) {
-        config.resolve.alias["@"] = path.resolve("./src");
-
-        return config;
-    },
 };
 
 module.exports = nextConfig;
