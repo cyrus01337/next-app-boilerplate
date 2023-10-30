7f898654f9b8f8ee35f331555a1691de01df22de
cyrus01337 <cyrus01337@gmail.com>
2023-10-30 14:09:12 +0000

    Use TypeScript

--- src/pages/index.jsx
+++
@@ -1,7 +0,0 @@
-import { Layout } from "@/components";
-
-export default function Home() {
-    return (
-        <Layout.Home title="Next App Boilerplate">Hello, world!</Layout.Home>
-    );
-}
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import Layout from "@/components/Layout";
+
+export default function Home() {
+    return <Layout.Home title="Next App Boilerplate">Hello, world!</Layout.Home>;
+}
