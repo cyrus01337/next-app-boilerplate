2b08379d196f91af7d83f23623bfb4a15a2acf30
cyrus01337 <cyrus01337@gmail.com>
2024-01-29 09:42:24 +0000

    Prefer loading font through next/font over Tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,7 @@
 const tailwindCSSTypography = require("@tailwindcss/typography");
-const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["src/**/*.tsx"],
     plugins: [tailwindCSSTypography],
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ["Inter", ...defaultTheme.fontFamily.sans],
-            },
-        },
-    },
 };
