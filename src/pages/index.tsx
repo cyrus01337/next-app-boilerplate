dbe46dcf7ceb57e2954d17024825992bd34b610d
cyrus01337 <cyrus01337@gmail.com>
2024-01-29 09:58:24 +0000

    Migrate to app router

--- src/pages/index.tsx
+++
@@ -1,5 +0,0 @@
-import Layout from "@/components/Layout";
-
-export default function Home() {
-    return <Layout.Home title="Next App Boilerplate">Hello, world!</Layout.Home>;
-}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,6 @@ const tailwindCSSTypography = require("@tailwindcss/typography");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["src/**/*.tsx"],
+    content: ["src/app/**/*.tsx"],
     plugins: [tailwindCSSTypography],
 };
