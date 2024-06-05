0b96c894a7525a01e818e274b6359343979ff671
cyrus01337 <cyrus21337@gmail.com>
2024-06-05 10:48:40 +0000

    Use TypeScript configuration

--- tailwind.config.cjs
+++
@@ -1,7 +0,0 @@
-const tailwindCSSTypography = require("@tailwindcss/typography");
-
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["src/**/*.tsx"],
-    plugins: [tailwindCSSTypography],
-};
