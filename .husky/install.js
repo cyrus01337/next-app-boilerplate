e7b46f5fdc31e3e03c28c1f2a4982251f62bb422
cyrus01337 <cyrus01337@gmail.com>
2024-07-26 06:52:49 +0000

    Disable Husky in pipeline

---
+++ .husky/install.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env bun
+import process from "process";
+
+if (process.env.NODE_ENV === "production" || process.env.CI === "true") {
+    process.exit(0);
+}
+
+const husky = (await import("husky")).default;
+
+// eslint-disable-next-line no-console
+console.log(husky());
