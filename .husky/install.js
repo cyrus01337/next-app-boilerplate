c639e56364ee491091e3b06dad8a837900b46282
cyrus01337 <cyrus21337@gmail.com>
2024-07-26 06:59:55 +0000

    Extend disable condition

--- .husky/install.js
+++ .husky/install.js
@@ -1,6 +1,11 @@
 import process from "process";
 
-if (process.env.NODE_ENV === "production" || process.env.CI === "true" || !process.env.HUSKY) {
+if (
+    process.env.NODE_ENV === "production" ||
+    process.env.CI === "true" ||
+    process.env.HUSKY === 0 ||
+    process.env.HUSKY === false
+) {
     console.log("Skipping Husky...");
 
     process.exit(0);
