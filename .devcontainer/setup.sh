64757440d108ef7cbd41f77a12d0f8a6b0094ed1
cyrus01337 <cyrus01337@gmail.com>
2024-10-09 02:31:35 +0100

    Link to external configurations

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,12 +1,1 @@
-#!/usr/bin/env bash
-set -e
-
-export BUN_INSTALL="/home/next/.bun"
-export PATH="$PATH:$BUN_INSTALL/bin"
-
-bun install
-bun pm trust --all || true
-
-if [ ! -d .husky/_ ]; then
-    bun run prepare
-fi
+../external/configurations/devcontainers/setup.sh
