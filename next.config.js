d3be57a1e27c62bc5d03e7ee7204678de2ed7f98
cyrus01337 <cyrus01337@gmail.com>
2026-01-13 13:57:45 +0000

    Ensure NextJS can see env packages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 export default {
+    devIndicators: false,
     output: "standalone",
     reactStrictMode: false,
+    // transpilePackages: ["@t3-oss/env-nextjs", "@t3-oss/env-core"],
 };
