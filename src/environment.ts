f5e1cd1a4ad0c0b8e020300a29c41fbbf1e741b9
cyrus01337 <cyrus01337@gmail.com>
2026-01-13 13:55:10 +0000

    Add placeholder type-safe environment handler

---
+++ src/environment.ts
@@ -0,0 +1,10 @@
+// import "client-only";
+
+// import { createEnv } from "@t3-oss/env-nextjs";
+// import { z as zod } from "zod";
+
+// export default createEnv({
+//     client: {},
+//     emptyStringAsUndefined: true,
+//     runtimeEnv: {},
+// });
---
+++ src/server/environment.ts
@@ -0,0 +1,10 @@
+// import "server-only";
+
+// import { createEnv } from "@t3-oss/env-nextjs";
+// import { z as zod } from "zod";
+
+// export default createEnv({
+//     server: {},
+//     emptyStringAsUndefined: true,
+//     runtimeEnv: {},
+// });
